0b9ceb91398967a6a23d56d8f0a5eb91df731b89
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2019-09-25 15:00:59 +0200

    gcp

--- samples/demo_run.py
+++ samples/demo_run.py
@@ -352,6 +352,7 @@ while True:
 
                 # check overlap
                 overlpaps = check_track_overlap(track_boxes)
+                print(overlaps)
                 if any(x > 0.4 for x in overlpaps):
                     overlaping_tracks.append(track)
 
