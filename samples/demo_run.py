e316e96cf807cf44bc8ef2d64d5167250fbf799b
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2019-09-26 14:35:28 +0200

    gcp

--- samples/demo_run.py
+++ samples/demo_run.py
@@ -342,7 +342,7 @@ while True:
                 
                 d = (int(bbox[0]), int(bbox[1]), int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3]))
                 #cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3])), (0, 0, 255), 2)
-                draw_track(frame, d, lbl, (0, 0, 255))
+                #draw_track(frame, d, lbl, (0, 0, 255))
                 cv2.imwrite('dbg.jpg', frame)
                 
             for row, col in zip(row_ind, col_ind):
